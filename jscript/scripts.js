15b2f50c652593d68708f7b2ecbc8b2bbf666dbe
Austin Reach <89221359+Kiikz@users.noreply.github.com>
2021-10-14 15:06:15 -0500

    Add files via upload

---
+++ jscript/scripts.js
@@ -0,0 +1,19 @@
+$(function() {
+
+    $('.cardClose1').click (function() {  
+      $('.cardHide').hide();
+    });
+  
+    $('.cardOpen1').click (function() {  
+      $('.cardHide').show();
+    });
+  
+    $('.cardClose2').click (function() {  
+      $('.cardHide').hide();
+    });
+  
+    $('.cardOpen2').click (function() {  
+      $('.cardHide').show();
+    });
+  
+  });
